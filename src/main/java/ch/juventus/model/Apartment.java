d1d049f2acfd3b1d0ae02c56a2fc708e0051f45b
Jannik <jannik.gysi@sonepar.ch>
2025-01-30 20:11:07 +0100

    Update Apartment.java

--- src/main/java/ch/juventus/model/Apartment.java
+++ src/main/java/ch/juventus/model/Apartment.java
@@ -5,9 +5,11 @@ import java.util.Map;
 public class Apartment {
 
     private final Map<String, Integer> rooms;
+    private final boolean rented;
 
-    public Apartment(Map<String, Integer> rooms) {
+    public Apartment(Map<String, Integer> rooms, boolean rented) {
         this.rooms = rooms;
+        this.rented = rented;
     }
 
 }
