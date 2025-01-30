f61dd387264b89f3de58e362ffc2ad5b3070fd3d
Jannik <jannik.gysi@sonepar.ch>
2025-01-30 20:24:00 +0100

    Update PropertyServiceTest.java

--- src/test/java/ch/juventus/service/PropertyServiceTest.java
+++ src/test/java/ch/juventus/service/PropertyServiceTest.java
@@ -53,4 +53,32 @@ class PropertyServiceTest {
         assertEquals(0, properties.size());
     }
 
+    @Test
+    void testGetAllFreeApartments() {
+        when(database.getAllProperties()).thenReturn(TestDataProvider.propertyList());
+        List<Apartment> freeApartments = propertyService.getAllFreeApartments();
+        assertEquals(4, freeApartments.size());
+        assertFalse(freeApartments.stream().anyMatch(Apartment::isRented));
+    }
+
+    @Test
+    void testGetAllPropertiesInCity() {
+        when(database.getAllProperties()).thenReturn(TestDataProvider.propertyList());
+        List<Property> propertiesInCity = propertyService.getAllPropertiesInCity("Altstetten");
+        assertEquals(2, propertiesInCity.size());
+        assertTrue(propertiesInCity.stream().allMatch(p -> p.getAddress().getCity().equalsIgnoreCase("Altstetten")));
+    }
+
+    @Test
+    void testGetAllApartmentsSortedByNumberOfRooms() {
+        when(database.getAllProperties()).thenReturn(TestDataProvider.propertyList());
+        List<Apartment> sortedApartments = propertyService.getAllApartmentsSortedByNumberOfRooms();
+        assertEquals(6, sortedApartments.size());
+        for (int i = 1; i < sortedApartments.size(); i++) {
+            int prevRooms = sortedApartments.get(i - 1).getRooms().values().stream().mapToInt(Integer::intValue).sum();
+            int currRooms = sortedApartments.get(i).getRooms().values().stream().mapToInt(Integer::intValue).sum();
+            assertTrue(prevRooms <= currRooms);
+        }
+    }
+
 }
